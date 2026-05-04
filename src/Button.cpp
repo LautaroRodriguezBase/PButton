3a040803e5428817092693034edb68ee4d4f5ac7
LERB <lerb.dev@gmail.com>
2026-05-04 02:02:07 -0300

    add src

---
+++ src/Button.cpp
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2026 Lautaro Ezequiel Rodriguez Base
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <sys/_stdint.h>
+#include "esp32-hal-gpio.h"
+#include "Button.h"
+
+Button::Button(uint8_t pin, uint8_t state2Read, uint8_t debounceDelay){
+    this->pin = pin;
+    this->state2Read = state2Read;
+    this->debounceDelay = debounceDelay;
+    this->lastButtonState = LOW;
+    this->lastDebounceTime = 0;
+    pinMode(pin, INPUT_PULLUP);
+}
+
+bool Button::read(){
+    bool res = false;
+    uint8_t reading = digitalRead(this->pin);
+
+    if (reading != this->lastButtonState) {
+        this->lastDebounceTime = millis();
+    }
+
+    if ((millis() - this->lastDebounceTime) > this->debounceDelay) {
+        if (reading != this->buttonState) {
+            this->buttonState = reading;
+            if (this->buttonState == this->state2Read) {
+                // Tal vez acá habría que agregar un código que
+                // lea si se mantuvo presionado por X cantidad de tiempo
+                res = true;
+            }
+        }
+    }
+
+    this->lastButtonState = reading;
+
+    return res;
+}
+
+uint8_t Button::getPin(){
+    return this->pin;
+}
---
+++ src/Button.h
@@ -0,0 +1,44 @@
+#ifndef BUTTON_h
+#define BUTTON_h
+
+/**
+ * Copyright 2026 Lautaro Ezequiel Rodriguez Base
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <sys/_stdint.h>
+#include <Arduino.h>
+
+#define TIME_DEBOUNCE_DELAY 50
+
+class Button{
+    private:
+        // the current reading from the input pin
+        uint8_t buttonState;
+        // the previous reading from the input pin
+        uint8_t lastButtonState;
+
+        uint64_t lastDebounceTime; // the last time the output pin was toggled
+        uint64_t debounceDelay;   // the debounce time; increase if the output flickers
+
+        uint8_t state2Read;
+        uint8_t pin;
+    public:
+        Button(uint8_t pin, uint8_t state2Read, uint8_t debounceDelay = TIME_DEBOUNCE_DELAY);
+
+        bool read();
+        uint8_t getPin();
+};
+
+#endif
